e9bec1fd7ca3079491b0cc474ab8362ea3e35009
strigonone <s15jaychao@hotmail.com>
2021-03-05 17:38:28 +1300

    Links working

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React from "react";
-import Nav from "./Components/nav";
-import Hsec1 from "./Components/hsec1";
-import Hsec2 from "./Components/hsec2";
-import Hsec3 from "./Components/hsec3";
-import Hsec4 from "./Components/hsec4";
 import "./App.css";
-// import { Route, Link } from "react-router-dom";
-import MainFooter from "./Components/Footer";
+import { Route } from "react-router-dom";
+import HomePage from "./Components/HomePage";
+import ProfilePage from "./Components/ProfilePage";
+import LogIn from "./Components/Login";
 
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Hsec1 />
-      <Hsec2 />
-      <Hsec3 />
-      <Hsec4/>
-      <div className="page-container">
-      <div className="content-wrap">
-
-      </div>
-      <MainFooter />
-      </div>
+      <Route exact path="/" component={HomePage}/>
+      <Route exact path="/ProfilePage" component={ProfilePage}/>
+      <Route exact path="/login" component={LogIn}/>
     </div>
 
     /*
-      <Route exact path="/" component={Home}/>
+
       <Route exact path="/about" component={About}/>
       <Route exact path="/projects" component={Projects}/>
       <Route exact path="/contact" component={Contacts}/>
--- src/Components/nav.js
+++ src/Components/nav.js
@@ -2,8 +2,7 @@ import React from 'react';
 import star from '../img/logo.png';
 import flagnz from '../img/nz.jpg';
 import flagmaori from '../img/maori.jpg';
-
-
+import { Link} from "react-router-dom";
 import '../CSS/nav.css'; 
 
 
@@ -20,7 +19,8 @@ function Nav(){
         <nav>
           <ul>
             <li>
-              <a href="#" className="a1">HOME </a>
+              {/* <a href="#" className="a1">HOME </a> */}
+              <Link to="/">Home</Link>
             </li>
             <li>
               <a href="#" className="a1">FEATURES </a>
@@ -28,6 +28,13 @@ function Nav(){
             <li>
               <a href="#" className="a1">TEACHERS </a>
             </li>
+
+<li>
+<Link to="/profilepage">Profile Page TEST LOL</Link> {/* Be sure to update this properly once finalized - Johnny */}
+</li>
+<li>
+<Link to="/login">JOSEPH LOGIN TEST </Link> {/* Be sure to update this properly once finalized - Johnny */}
+</li>
           </ul>
         </nav>
 
