6a2680174f0fe3b9ecd4be9b5ed02c2b5bedb58d
vuso2021 <ofa.faletau@outlook.com>
2021-03-06 07:05:56 +1300

    update slide

--- src/Components/slide.js
+++ src/Components/slide.js
@@ -2,37 +2,16 @@ import React from "react";
 import "../CSS/slide.css";
 
 
-import React from 'react';
-import { Slide } from 'react-slideshow-image';
 
-const slideImages = [
-  'images/slide_2.jpg',
-  'images/slide_3.jpg',
-  'images/slide_4.jpg'
-];
 
-const Slideshow = () => {
+function Slideshow (){
     return (
-      <div>
-        <Slide easing="ease">
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-              <span>Slide 1</span>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-              <span>Slide 2</span>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-              <span>Slide 3</span>
-            </div>
-          </div>
-        </Slide>
-      </div>
-    )
-};
+
+        <div>
+            I'm going to use this space to test slideshow
+        </div>
+    );
+
+}
 
 export default Slideshow;
