21798d6d0cfc9c26aef4826dd25abc5d7708852f
Your Name <you@example.com>
2021-03-07 11:36:55 +1300

    have moved all css for login/signup to one page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../CSS/login.css'
+import '../CSS/SignUp.css'
 import students from '../Students.jpg'
 import teachers from '../teacher-1.jpg'
 import {Link } from 'react-router-dom'
@@ -8,11 +8,11 @@ import {Link } from 'react-router-dom'
 function LogIn() {
   return (
     // needs a better class name now I've found out that signup needs to go here too
-    <div className='Login'>
+    <div className='Form'>
 
       {/* <h1>LOG IN</h1>    */}
       <div className='StudentLogIn'>
-        <img src={students} className="LogInPic" alt="Students" />
+        <img src={students} className="FormPic" alt="Students" />
         <p>Students</p>
         {/* these a tags will be changed to Link once we have added in the newer pages */}
         <a href='#'>Log In</a> <Link to='/SignUp'>Sign Up</Link>
@@ -21,13 +21,13 @@ function LogIn() {
           <input placeholder='Email Address'></input>
           <input placeholder="Password"></input>
           {/* have added link to the student project library page. User data has not been added yet */}
-          <Link to="/StudentProjectLib"><button className="LogInButton">Log In</button></Link>
+          <Link to="/StudentProjectLib"><button className="FormButton">Log In</button></Link>
         </div>
         </div>
       </div>
 
       <div className='TeacherLogIn'>
-        <img src={teachers} className="LogInPic" alt="Teacher" />
+        <img src={teachers} className="FormPic" alt="Teacher" />
         <p>Teachers</p>
         {/* these a tags will be changed to Link once we have added in the newer pages */}
         <a href='#'>Log In</a> <Link to='/SignUp'>Sign Up</Link>
@@ -36,7 +36,7 @@ function LogIn() {
           <input type='email' required placeholder='Email Address'></input>
           <input placeholder="Password"></input>
           {/* Will be changed to teachers page once the page has been made */}
-          <Link to="/StudentProjectLib"><button className="LogInButton">Log In</button></Link>
+          <Link to="/teacherpro"><button className="FormButton">Log In</button></Link>
           </div>
           
         </div>
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -7,29 +7,31 @@ import { Link } from 'react-router-dom';
 function SignUp(){
     return(
 
-        <div className ='SignUp'>
-            <div className='StudentLogIn'>
-            <img src={students} className='LogInPic' alt='Students' />
+        <div className ='Form'>
+            <div className='StudentSignUp'>
+            <img src={students} className='FormPic' alt='Students' />
             <p>Students</p>
         {/* these a tags will be changed to Link once we have added in the newer pages */}
         <Link to='/LogIn'>Log In</Link> <Link to='/SignUp'>Sign Up</Link>
         <div className='UserInput'>
-        <div className='SignUpInput'>
+        
         <input placeholder='Full Name'></input>
         <input placeholder='Email Address' name='email' required></input>
         <input placeholder='Password'></input>
         <input placeholder='Confirm Password'></input>
           {/* have added link to the student project library page. User data has not been added yet */}
-          <Link to="/LogIn"><button className="LogInButton">Sign Up</button></Link>
-        </div>
+          <Link to="/LogIn"><button className="FormButton">Sign Up</button></Link>
+        
         </div>
       </div>
 
-      <div className='TeacherLogIn'>
-        <img src={teachers} className="LogInPic" alt="Teacher" />
+      <div className='TeacherSignUp'>
+        <img src={teachers} className="FormPic" alt="Teacher" />
         <p>Teachers</p>
         {/* these a tags will be changed to Link once we have added in the newer pages */}
+        <div className='FormLink'>
         <Link to='/LogIn'>Log In</Link> <Link to='/SignUp'>Sign Up</Link>
+        </div>
         <div className='UserInput'>
         <div className='SignUpInput'>
         <input placeholder='Full Name'></input>
@@ -37,7 +39,7 @@ function SignUp(){
         <input placeholder='Password'></input>
         <input placeholder='Confirm Password'></input>
           {/* Will be changed to teachers page once the page has been made */}
-          <Link to="/LogIn"><button className="LogInButton">Sign Up</button></Link>
+          <Link to="/LogIn"><button className="FormButton">Sign Up</button></Link>
           </div>
           </div>
         </div>
