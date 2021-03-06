f891b8df5f96e8d8856f0721d870cba6f995a8e7
Your Name <you@example.com>
2021-03-06 19:25:57 +1300

    Updated Student Project Library and added grid gallery

--- src/StudentProjectLib.js
+++ src/StudentProjectLib.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Nav from './Components/nav.js';
 import './StudentProjectLib.css';
-import project1 from './img/project1.png';
+import project1 from './img/Project -1@2x.jpg';
+import project2 from './img/Project -2@2x.jpg';
+import project3 from './img/Project -3@2x.jpg';
+import project4 from './img/Project -4@2x.jpg';
+import project5 from './img/Project -5@2x.jpg';
+import project6 from './img/Project -6@2x.jpg';
 
 
 function StudentProjectLib() {
@@ -9,41 +14,51 @@ function StudentProjectLib() {
         <div className="StudentProjectLibrary">
             <Nav />
             <div className='MainPage'>
-                <div className='SideBar'>
+                <div className='ProjectSideBar'>
                     <ul>
-                        <div className="Subscription">
+                        <div className="ListHeader">
                             <li>Subsrciption</li>
-
-                            <label class="container">One
-                        <input type="checkbox" />
-                                <span class="checkmark"></span>
-                            </label>
-
-                            <label class="container">One
-                        <input type="checkbox" />
-                                <span class="checkmark"></span>
-                            </label>
-
                         </div>
-                        <li>Activity Type</li>
-                        <li>Year Level</li>
+                        <div className="ListHeader">
+                            <li>Activity Type</li>
+                        </div>
+                        <div className="ListHeader">
+                            <li>Year Level</li>
+                        </div>
                     </ul>
-                </div>
+                </div>   
 
-                <div className='Header'>
-                    <h1>Projects</h1>
-                    <div className="ProjectGallery">
+                <div className='ProjectContainer'>
+                   
+                    <div className ='ProjectLibText'>
+                        <h1>Projects</h1>
                         <p>This is where all the project pictures will go</p>
-                        <img src={project1} className='Project1' alt="Project 1" />
-                        <img src={project1} className='Project1' alt="Project 1" />
-                        <img src={project1} className='Project1' alt="Project 1" />
-                        <img src={project1} className='Project1' alt="Project 1" />
-                        <img src={project1} className='Project1' alt="Project 1" />
-                        <img src={project1} className='Project1' alt="Project 1" />
-                        <img src={project1} className='Project1' alt="Project 1" />
-                        <img src={project1} className='Project1' alt="Project 1" />
-                        <img src={project1} className='Project1' alt="Project 1" />
-
+                    </div>
+                        <div className="ProjectGallery">
+                        <div className = 'ProjectPic1'>
+                        <img src={project1} className='ProjectPic1' alt="Project 1" />
+                        <h3>Introduction</h3><p>BEGINNER | Animation</p>
+                        </div>
+                        <div className = 'ProjectPic2'>
+                        <img src={project2} className='ProjectPic2' alt="Project 2" />
+                        <h3>My Birthday</h3><p>BEGINNER | Animation</p>
+                        </div>
+                        <div className = 'ProjectPic3'>
+                        <img src={project3} className='ProjectPic3' alt="Project 3" />
+                        <h3>10 Block Challenge</h3><p>BEGINNER | Animation</p>
+                        </div>
+                        <div className = 'ProjectPic4'>
+                        <img src={project4} className='ProjectPic4' alt="Project 4" />
+                        <h3>Build a Band</h3><p>BEGINNER | Animation</p>
+                        </div>
+                        <div className = 'ProjectPic5'>
+                        <img src={project5} className='ProjectPic5' alt="Project 5" />
+                        <h3>The bear and the monkey</h3><p>Project5</p>
+                        </div>
+                        <div className = 'ProjectPic6'>
+                        <img src={project6} className='ProjectPic6' alt="Project 6" />
+                        <h3>Debugging</h3><p>BEGINNER | Animation</p>
+                        </div>
                     </div>
                 </div>
 
