c979c3e4fb979e6ad280a62c827cef05ef7c5b3b
strigonone <s15jaychao@hotmail.com>
2021-03-07 23:49:27 +1300

    Uploading new content

---
+++ src/Components/Footer2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainFooter2 () {
+    return (
+<footer>
+      <p style={{textAlign:'center'}}> © LevelUp Works 2020 </p>
+</footer>
+    );
+}
+
+export default MainFooter2
---
+++ src/Components/TopNavPart2.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../CSS/TopNavPart2.css";
+
+import star from "../img/logo.png";
+import flagnz from "../img/nz.jpg";
+import flagmaori from "../img/maori.jpg";
+
+function TopNav2() {
+    return (
+<div className="topNav2">
+        {/*Logo */}
+        <div className="topLogo2">
+          <img src={star} className="star" alt="starlogo" />
+        </div>
+        {/*nz flag and maori flag */}
+        <div className="topFlagLang2">
+            <img src={flagnz} alt="NZ Flag" />{" "}
+            <img src={flagmaori} alt="Maori Flag" />
+        </div>
+        </div>
+    );
+}
+
+export default TopNav2;
