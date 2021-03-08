024085dd9cb8b007208331cd914c80f58ea0cbb6
strigonone <s15jaychao@hotmail.com>
2021-03-09 01:33:37 +1300

    Sidebar collapsed prototype

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import { Route } from "react-router-dom";
 import HomePage from "./Components/HomePage";
 import ProfilePage from "./Components/ProfilePage";
 import LogIn from "./Components/Login";
-import StudentProjectLib from './Components/StudentProjectLib.js';
-import SignUp from './Components/SignUp.js'
-import Teacherpro from './Components/teacherpro.js';
+import StudentProjectLib from "./Components/StudentProjectLib.js";
+import SignUp from "./Components/SignUp.js";
+import Teacherpro from "./Components/teacherpro.js";
 import TeacherStudentProfile from "./Components/TeacherStudentProfilePart1";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={HomePage}/>
-      <Route exact path="/ProfilePage" component={ProfilePage}/>
-      <Route exact path="/Login" component={LogIn}/>
-      <Route exact path="/SignUp" component={SignUp}/>
-      <Route exact path="/StudentProjectLib" component={StudentProjectLib}/>
-      <Route exact path="/teacherpro" component={Teacherpro}/>
-      <Route exact path="/teacherstudentprofile" component={TeacherStudentProfile} />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/ProfilePage" component={ProfilePage} />
+      <Route exact path="/Login" component={LogIn} />
+      <Route exact path="/SignUp" component={SignUp} />
+      <Route exact path="/StudentProjectLib" component={StudentProjectLib} />
+      <Route exact path="/teacherpro" component={Teacherpro} />
+      <Route
+        exact
+        path="/teacherstudentprofile"
+        component={TeacherStudentProfile}
+      />
     </div>
 
     /*
@@ -30,7 +34,6 @@ function App() {
       this is was inside the return function
     */
   );
-
 }
 
 export default App;
--- src/Components/TeacherStudentProfilePart1.js
+++ src/Components/TeacherStudentProfilePart1.js
@@ -1,32 +1,32 @@
 import React from "react";
 import MainFooter from "./Footer";
-import TopNav2 from './TopNavPart2';
-import '../CSS/ProfileMainBackground.css';
-import MainFooter2 from './Footer2';
-
+import TopNav2 from "./TopNavPart2";
+import "../CSS/ProfileMainBackground.css";
+import MainFooter2 from "./Footer2";
+import SideNavMainTest from "./SideNavMainTest";
 
 function TeacherStudentProfile() {
-    return (
-      <div className="webcontainer">
-<TopNav2 />
-
-<div className="profile-main-container">
-<div className="profile-inner-container">
-<h1>Hello plz halp me. Plz turn me into a reusable container / componenet somehow PLEASEEEEEEE</h1>
-
+  return (
+    <div className="webcontainer">
+      <TopNav2 />
+      {/* <SideNavMainTest /> */}
+      <div className="profile-main-container">
+        <div className="profile-inner-container">
+          <h1>
+            Hello plz halp me. Plz turn me into a reusable container /
+            componenet somehow PLEASEEEEEEE
+          </h1>
         </div>
-        </div>
+      </div>
 
-{/* Turn that into a componenet and wrap around it somehow so it is reusable for other content ^^^ */}
+      {/* Turn that into a componenet and wrap around it somehow so it is reusable for other content ^^^ */}
 
+      {/* Temp code so I can switch back and forth between profile info page and teachers students page */}
+      <MainFooter />
 
-{/* Temp code so I can switch back and forth between profile info page and teachers students page */}
-<MainFooter />
-
-<MainFooter2 />
-</div>
-
-    );
+      <MainFooter2 />
+    </div>
+  );
 }
 
 export default TeacherStudentProfile;
