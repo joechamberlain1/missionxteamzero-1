79b5a83c754c3e9f15b358387a165c4ff140beb0
vuso2021 <ofa.faletau@outlook.com>
2021-03-10 12:31:18 +1300

    add CSS to Nav and add font: Nunito

--- src/App.js
+++ src/App.js
@@ -25,14 +25,6 @@ function App() {
       />
     </div>
 
-    /*
-
-      <Route exact path="/about" component={About}/>
-      <Route exact path="/projects" component={Projects}/>
-      <Route exact path="/contact" component={Contacts}/>
-
-      this is was inside the return function
-    */
   );
 }
 
