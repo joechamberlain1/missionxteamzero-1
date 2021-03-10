77bfdab73dbd1249ad953580374a4fd375bc0459
joechamberlain1 <joechamberlain2801@gmail.com>
2021-03-11 12:07:54 +1300

    project gallery additions

---
+++ src/Components/StudentProjectContent.js
@@ -0,0 +1,36 @@
+import project1 from '../img/Project -1@2x.jpg';
+import project2 from '../img/Project -2@2x.jpg';
+import project3 from '../img/Project -3@2x.jpg';
+import project4 from '../img/Project -4@2x.jpg';
+import project5 from '../img/Project -5@2x.jpg';
+import project6 from '../img/Project -6@2x.jpg';
+
+
+export default ProjectCardInfo [
+{
+    Image = <img src = {project1}/>
+    title = 
+},
+
+{
+    Image = <img src = {project2}/>
+},
+
+{
+    Image = <img src = {project3}/>
+},
+
+{
+    Image = <img src = {project4}/>
+},
+
+{
+    Image = <img src = {project5}/>
+},
+
+{
+    Image = <img src = {project6}/>
+},
+
+
+];
