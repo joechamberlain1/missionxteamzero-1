e00ffcf64f1fbb07ff6db4bfdd36e94cc88cf3d3
Your Name <you@example.com>
2021-03-10 20:34:04 +1300

    Have updated CSS with log in /sign up modal

--- src/Components/SideNavTest.js
+++ src/Components/SideNavTest.js
@@ -1,42 +1,42 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import * as ImIcons from "react-icons/im";
-import { SideNavData } from "./SideNavData";
-import "../CSS/SideNavTest.css";
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import * as ImIcons from "react-icons/im";
+// import { SideNavData } from "./SideNavData";
+// import "../CSS/SideNavTest.css";
 
-function SideNavTest() {
-  const [sidebar, setSidebar] = useState(false);
+// function SideNavTest() {
+//   const [sidebar, setSidebar] = useState(false);
 
-  const showSideBar = () => setSidebar(!sidebar);
+//   const showSideBar = () => setSidebar(!sidebar);
 
-  return (
-    <>
-      <div className="navbar">
-        <Link to="#" className="menu-bars">
-          <ImIcons.ImArrowRight onClick={showSideBar} />
-        </Link>
-      </div>
-      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-        <ul className="nav-menu-items" onClick={showSideBar}>
-          <li className="navbar-toggle">
-            <Link to="#" className="menu-bars">
-              <ImIcons.ImArrowLeft />
-            </Link>
-          </li>
-          {SideNavData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <div className="navbar">
+//         <Link to="#" className="menu-bars">
+//           <ImIcons.ImArrowRight onClick={showSideBar} />
+//         </Link>
+//       </div>
+//       <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+//         <ul className="nav-menu-items" onClick={showSideBar}>
+//           <li className="navbar-toggle">
+//             <Link to="#" className="menu-bars">
+//               <ImIcons.ImArrowLeft />
+//             </Link>
+//           </li>
+//           {SideNavData.map((item, index) => {
+//             return (
+//               <li key={index} className={item.cName}>
+//                 <Link to={item.path}>
+//                   {item.icon}
+//                   <span>{item.title}</span>
+//                 </Link>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </nav>
+//     </>
+//   );
+// }
 
-export default SideNavTest;
+// export default SideNavTest;
