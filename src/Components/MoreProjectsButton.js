38ad598114ddf1967607681b4662863ab69f6460
strigonone <s15jaychao@hotmail.com>
2021-03-12 00:59:12 +1300

    Added seprate button componenet for teacher page

---
+++ src/Components/MoreProjectsButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../CSS/MoreProjectsButton.css";
+
+function MoreProjectsButton() {
+  return <button className="moreprojectsbutton">More Projects</button>;
+}
+
+export default MoreProjectsButton;
