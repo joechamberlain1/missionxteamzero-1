dbccd80498f11f6dc4d9f329017b066414e70875
strigonone <s15jaychao@hotmail.com>
2021-03-12 00:59:12 +1300

    Added seprate button componenet for teacher page

---
+++ src/Components/HelpCenterButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../CSS/HelpCenterButton.css";
+
+function HelpCenterButton() {
+  return <button className="helpcenterbutton">Help Center</button>;
+}
+
+export default HelpCenterButton;
---
+++ src/Components/TakeScreenShotButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../CSS/TakeScreenShotButton.css";
+
+function TakeScreenShotButton() {
+  return <button className="TakeScreenshot">Take Screenshot</button>;
+}
+
+export default TakeScreenShotButton;
