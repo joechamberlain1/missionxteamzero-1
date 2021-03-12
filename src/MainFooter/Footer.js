74fd4534b2efc7ba56c34290ac035cf73f4b6ec5
strigonone <s15jaychao@hotmail.com>
2021-03-13 01:13:00 +1300

    Moved MainFooter into its own folder

---
+++ src/MainFooter/Footer.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+const MainFooter = () => {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          {/* Column1 */}
+          <div className="Col">
+            <h4>COMPANY</h4>
+            <ul className="list-unstyled">
+              <li>About Us</li>
+              <li>Careers</li>
+              <li>Partners</li>
+              <li>
+                <Link to="/teacher1">Teacher</Link>{" "}
+                {/* use to access to my teacher1 js - Ofa */}
+              </li>
+
+              <li>
+                <Link to="/helpT">Help</Link>{" "}
+                {/* use to access to my teacher2 js - Ofa */}
+              </li>
+
+              <li>
+                <Link to="/teacherstu">TeacherStudent</Link>{" "}
+                {/* use to access to my teacher2 js - Ofa */}
+              </li>
+
+              <li>
+                <Link to="./Project/learning">Learning Page</Link>{" "}
+                {/* use to access to my teacher2 js - Ofa */}
+              </li>
+            </ul>
+          </div>
+
+          {/* Column2 */}
+          <div className="Col">
+            <h4>COURSES</h4>
+            <ul className="list-unstyled">
+              <li>Register</li>
+              <li>Login</li>{" "}
+              {/*Once website is functioning, we should link these to whatever web pages we have live. Login / Register is one of them */}
+              <li>Projects</li>
+              <li>Teachers</li>
+              <li>Parents</li>
+              <li>Resources</li>
+            </ul>
+          </div>
+          {/* Column3 */}
+          <div className="Col">
+            <h4>SUPPORT</h4>
+            <ul className="list-unstyled">
+              <li>FAQs</li>
+              <li>Helpdesk</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          {/* Column4 */}
+          <div className="Col">
+            <h4>LEGAL</h4>
+            <ul className="list-unstyled">
+              <li>Terms & Conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          {/* Column5 */}
+          <div className="Col">
+            <h4>LevelUp Works</h4>
+            <p>
+              LevelUp Works is an Auckland-based enterprise dedicated to
+              developing game-based learning software to help teachers in
+              response to the New Zealand Digital Technologies & Hangarau
+              Matchiko. alan@levelupworks.com (021) 668 185
+            </p>
+          </div>
+          {/* Column6 */}
+          <div className="Col">
+            <h4>Random Test</h4>
+            <ul className="list-unstyled">
+              <li>
+                <Link to="/profilepage">Profile Page TEST LOL</Link>{" "}
+                {/* Be sure to update this properly once finalized - Johnny */}
+              </li>
+              <li>
+                <Link to="/TeacherStudentProfilePart1">
+                  Teacher Student Profile Page TEST LOL
+                </Link>{" "}
+                {/* Be sure to update this properly once finalized - Johnny */}
+              </li>
+              <li>
+                <Link to="/StudentProfileGrid">StudentGrid thingy</Link>{" "}
+                {/* Be sure to update this properly once finalized - Johnny */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainFooter;
