2258294feea5b184903e0e6219da525a47cc14d9
vuso2021 <ofa.faletau@outlook.com>
2021-03-13 17:54:54 +1300

    update dashbord.js - ofa

--- src/App.js
+++ src/App.js
@@ -15,12 +15,16 @@ import StudentProjectLib from "./joe/StudentProjectLib.js";
 import ProjectBuilderSideBar from './joe/ProjectBuilderSideBar';
 import StudentProjectBuilder from './joe/StudentProjectBuilder';
 
-import Checkboxes from "./ofa/checkbox";
-import Helpt from "./ofa/help-request";
-import Teacher1 from "./ofa/progress-tracker";
-import Teacher2 from "./ofa/teacher2";
+
+
+import HelpRequest from "./ofa/help-request";
+import ProgressTracker from "./ofa/progress-tracker";
 import DashboardProgress from "./ofa/dashboard-progress";
 import DashboardHelp from "./ofa/dashboard-help";
+import Checkboxes from "./ofa/checkbox";
+import RadioButton from "./ofa/radio-button";
+
+
 import TeacherDashboardTest from "./ofa/dashboard";
 
 
@@ -33,9 +37,9 @@ function App() {
       <Route exact path="/SignUp" component={SignUp} />
       <Route exact path="/StudentProjectLib" component={StudentProjectLib} />
       <Route exact path="/dashboard-progress" component={DashboardProgress} />
-      <Route exact path="/progress-tracker" component={Teacher1} />
-      <Route exact path="/teacher2" component={Teacher2} />
-      <Route exact path="/help-request" component={Helpt} />
+      <Route exact path="/progress-tracker" component={ProgressTracker} />
+      <Route exact path="/radio-button" component={RadioButton} />
+      <Route exact path="/help-request" component={HelpRequest} />
       <Route exact path="/ImageAvatars" component={ImageAvatars} />
       <Route exact path="/checkbox" component={Checkboxes} />
       <Route exact path="/dashboard-help" component={DashboardHelp} />
