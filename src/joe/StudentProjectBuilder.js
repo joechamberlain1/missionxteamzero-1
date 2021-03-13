b15bfed7bbc638d592157f164bb95a562fe50118
Joe Chamberlain <you@example.com>
2021-03-13 14:37:46 +1300

    Updated StudentProjectBuilder

--- src/joe/StudentProjectBuilder.js
+++ src/joe/StudentProjectBuilder.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import TopNav2 from '../TeacherDashboard/TopNavPart2';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import ProjectBuilderSideBar from './ProjectBuilderSideBar'
+import React from "react";
+import ProjectBuilderSideBar from "./ProjectBuilderSideBar";
+import TopNav2 from "../TeacherDashboard/TopNavPart2";
+import MainFooter2 from "../TeacherDashboard/Footer2";
+import HelpCenterButton from "../TeacherDashboard/HelpCenterButton";
+import MoreProjectsButton from "../TeacherDashboard/MoreProjectsButton";
+import TakeScreenShotButton from "../TeacherDashboard/TakeScreenShotButton";
+import "../TeacherDashboard/MainTemplate.css";
 
-function StudentProjectBuilder(){
-    return(
-        <div><TopNav2 />
-        <h1>HELLO<ArrowBackIosIcon /></h1>
+// Use this as a template when creating a page
+
+function MainTemplate() {
+  return (
+    <div className="webcontainer">
+      <TopNav2 />
+
+      <div className="profile-main-container">
         <ProjectBuilderSideBar />
+        <div className="ButtonContainer">
+          <TakeScreenShotButton />
+          <HelpCenterButton />
+          <MoreProjectsButton />
         </div>
-    )
+        <div className="profile-maininner-container"></div>
+      </div>
+
+      {/* Turn that into a componenet and wrap around it somehow so it is reusable for other content ^^^ */}
+
+      {/* Temp code so I can switch back and forth between profile info page and teachers students page */}
+      {/* <MainFooter /> */}
+
+      <MainFooter2 />
+    </div>
+  );
 }
 
-export default StudentProjectBuilder
+export default MainTemplate;
