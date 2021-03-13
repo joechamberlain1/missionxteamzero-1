8421004f8140dc0a06fb5ebfabb536d9aebc01bc
vuso2021 <ofa.faletau@outlook.com>
2021-03-14 11:32:01 +1300

    update leftnavl2 - ofa

--- src/ofa/leftNavLayout2.js
+++ src/ofa/leftNavLayout2.js
@@ -1,7 +1,8 @@
 import React from "react";
-import "../ofa/css/learning.css";
+import "../ofa/css/leftNavLayout2.css";
 import { Link } from "react-router-dom";
 import { ProIcons } from "./components/icons";
+import { SetIcon } from "./components/icons2";
 import Tavitar from "./components/tavitar";
 import openicon from "../ofa/img/open.png";
 import closeicon from "../ofa/img/close.png";
@@ -13,6 +14,7 @@ function LeftNavL2() {
     <div className="mainL">
               
                 <Tavitar />
+
 
                 {/*left-side menu */}
                 {ProIcons.map((item, index) => {
@@ -26,14 +28,26 @@ function LeftNavL2() {
                   );
                 })}
 
-
                 
                 {/* open button */}     
                 <div className="arrowopen">
                   <Link to="#"><img src={openicon} /></Link>
                 </div>
           
-    
+
+                {/*left-side bottom menu*/}
+                <div className="bottomicon">
+                {SetIcon.map((item, index) => {
+                        return (
+                          <li key={index} className={item.cName}>
+                            <Link to={item.path}>
+                              {item.icon}
+                              <span>{item.title}</span>
+                            </Link>
+                          </li>
+                        );
+                      })}
+                </div>
     </div>
   
   
