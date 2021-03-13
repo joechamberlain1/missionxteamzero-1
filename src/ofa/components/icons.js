c5fe5ccc41ba1f5e387d41ed9c6a57674e8e6d14
vuso2021 <ofa.faletau@outlook.com>
2021-03-14 11:32:01 +1300

    update leftnavl2 - ofa

--- src/ofa/components/icons.js
+++ src/ofa/components/icons.js
@@ -1,43 +1,16 @@
 import React from "react";
-
-
+import "../css/icons.css";
 import progressicon from "../img/project-icons/progress-dark.png";
 import studenticon from "../img/project-icons/student-dark.png";
 import helpicon from "../img/project-icons/help-dark.png";
 import subicon from "../img/project-icons/submissions-dark.png";
 import libraryicon from "../img/project-icons/library-light.png";
 
-
 export const ProIcons = [
-  {
-    title: "",
-    path: "/dashboard",
-    icon: <img src={progressicon} />,
-    cName: "nav-text",
-  },
-  {
-    title: "",
-    path: "/dashboard",
-    icon: <img src={studenticon} />,
-    cName: "nav-text",
-  },
-  {
-    title: "",
-    path: "/dashboard",
-    icon: <img src={helpicon} />,
-    cName: "nav-text",
-  },
-  {
-    title: "",
-    path: "/dashboard",
-    icon: <img src={subicon} />,
-    cName: "nav-text",
-  },
-  {
-    title: "",
-    path: "/dashboard",
-    icon: <img src={libraryicon} />,
-    cName: "nav-text",
-  },
+  { title: "", path: "/leftNavLayout2", icon: <img src={progressicon} />, cName: "layicon", },
+  { title: "", path: "/leftNavLayout2", icon: <img src={studenticon} />, cName: "layicon", },
+  { title: "", path: "/leftNavLayout2", icon: <img src={helpicon} />, cName: "layicon", },
+  { title: "", path: "/leftNavLayout2", icon: <img src={subicon} />, cName: "layicon", },
+  { title: "", path: "/leftNavLayout2", icon: <img src={libraryicon} />, cName: "layicon", },
 ];
 
---
+++ src/ofa/components/icons2.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../css/icons2.css";
+
+
+export const SetIcon = [
+  { title: "", path: "/leftNavLayout2", icon: <i class="fa fa-sign-out" aria-hidden="true"></i>, cName: "layicon2", },
+  { title: "", path: "/leftNavLayout2", icon: <i class="fa fa-cog" aria-hidden="true"></i>, cName: "layicon2", },
+  { title: "", path: "/leftNavLayout2", icon: <i class="fa fa-user-circle" aria-hidden="true"></i>, cName: "layicon2", },
+
+];
