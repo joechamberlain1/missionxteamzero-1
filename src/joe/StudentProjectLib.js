0112ea7236473b0c35b1aa9699ef96f10c86a044
Joe Chamberlain <you@example.com>
2021-03-13 18:07:53 +1300

    Updated buttons in student library

--- src/joe/StudentProjectLib.js
+++ src/joe/StudentProjectLib.js
@@ -8,24 +8,22 @@ import MainFooter from '../MainFooter/Footer.js'
 import { Button, } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles'
 import { Link } from 'react-router-dom';
-import StudentProjectBuilder from './StudentProjectBuilder'
+
 
 // import { yellow } from '@material-ui/core/colors';
 
 // this controls the button at the botttom and still needs some configuring
 const useStyles = makeStyles((theme) => ({
     paper: {
-        position: "static",
+        
         display: "flex",
         float: "right",
-        flexDirection: "column",
-        backgroudColor: "yellow",
         margin: 50,
         boxShadow: "none",
         border: "1px solid",
         backgroundColor: "#E5AB2C",
         color: "white",
-        width: 170,
+
 
         "&:hover": {
             backgroundColor: "#E5AB2C",
@@ -39,7 +37,7 @@ function StudentProjectLib() {
     const classes = useStyles();
 
     return (
-        <div>
+        
         <div className="StudentProjectLibrary">
             <Nav />
             <div className='MainPage'>
@@ -56,7 +54,7 @@ function StudentProjectLib() {
 
                         <h1>PROJECTS</h1>
                         <p>Welcome to the project library. You can use filters on the right to help you search for specific projects.</p>
-                        <Link to='/StudentProjectBuilder'> TAKE ME TO MY SIDEBAR</Link>
+                        
                         <LibButton />
 
                     </div>
@@ -74,31 +72,7 @@ function StudentProjectLib() {
             <MainFooter />
 
         </div>
-        <div className="ProjectContainer">
-          <div className="ProjectLibText">
-            <h1>PROJECTS</h1>
-            <p>
-                    Welcome to the project library. You can use filters on the right
-                    to help you search for specific projects.
-            </p>
-
-            <LibButton />
-          </div>
-          <div>
-            <ProjectGallery />
-          </div>
-          <div>
-            <Button
-              href="#top"
-              variant="contained"
-              className={classes.paper}
-              size="small">
-                    Back to the top
-            </Button>
-          </div>
-        </div>
-        <MainFooter />
-    </div >
+        
   );
 }
 
