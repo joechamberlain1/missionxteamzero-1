e105a52dd3eb7a4f32709b043f48fa9360559e69
strigonone <s15jaychao@hotmail.com>
2021-03-14 23:09:40 +1300

    Initial Stages of Teacher Projects Page

---
+++ src/TeacherProjects/TeacherProjectsPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Nav from "../Components/nav.js";
+import MainFooter from "../MainFooter/Footer.js";
+
+function TeacherProjectsPage() {
+  return (
+    <div className="webcontainer">
+      <Nav />
+
+      <div className="main-projectcontainer">
+        <div className="projectSideBar">
+          <h1>hello</h1>
+        </div>
+        <div className="projectContent">
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+          <h1>Hello</h1>
+        </div>
+      </div>
+
+      <MainFooter />
+    </div>
+  );
+}
+
+export default TeacherProjectsPage;
