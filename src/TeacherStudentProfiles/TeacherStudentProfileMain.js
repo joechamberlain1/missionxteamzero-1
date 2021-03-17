27bf19df2a16f82a8f5a62b9fa8f5126e5e4975c
strigonone <s15jaychao@hotmail.com>
2021-03-18 01:32:55 +1300

    Small update on ProfilePage styling

--- src/TeacherStudentProfiles/TeacherStudentProfileMain.js
+++ src/TeacherStudentProfiles/TeacherStudentProfileMain.js
@@ -1,8 +1,6 @@
 import React from "react";
-import TopNav2 from "../TeacherDashboard/TopNavPart2";
 import "../TeacherDashboard/MainTemplate.css";
 import MainFooter2 from "../TeacherDashboard/Footer2";
-import MainFooter from "../MainFooter/Footer.js";
 import "../JohnnyTest/SideNavTest.css";
 import StudentProfileGrid from "./StudentProfileGrid";
 import SideNavTestv2 from "../JohnnyTest/SideNavTestv2";
@@ -13,7 +11,6 @@ function TeacherStudentProfileMain() {
   return (
     <div className="webcontainer">
       <TopNav3 />
-
       <div className="profile-main-container">
         <SideNavTestv2 />
         <div className="DashboardContent">
@@ -43,11 +40,6 @@ function TeacherStudentProfileMain() {
           </div>
         </div>
       </div>
-
-      {/* Turn that into a componenet and wrap around it somehow so it is reusable for other content ^^^ */}
-
-      {/* Temp code so I can switch back and forth between profile info page and teachers students page */}
-      {/* <MainFooter /> */}
 
       <MainFooter2 />
     </div>
