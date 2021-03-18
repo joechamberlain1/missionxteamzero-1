8d059437798ee8fafc5c2ede1d626c9c942195d5
strigonone <s15jaychao@hotmail.com>
2021-03-18 23:01:32 +1300

    Updated MoreProjectsButton to link to next page

--- src/TeacherDashboard/MoreProjectsButton.js
+++ src/TeacherDashboard/MoreProjectsButton.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./MoreProjectsButton.css";
+import { Link } from "react-router-dom";
 
 function MoreProjectsButton() {
-  return <button className="moreprojectsbutton">More Projects</button>;
+  return (
+    <button className="moreprojectsbutton">
+      <Link to="/TeacherProjectsPage">More Projects</Link>
+    </button>
+  );
 }
 
 export default MoreProjectsButton;
