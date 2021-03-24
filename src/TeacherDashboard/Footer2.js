30e7ceb394ba81126bad52fa06ab618f4d6b082a
strigonone <s15jaychao@hotmail.com>
2021-03-24 23:47:10 +1300

    Moved footer2 css into the maintemplate css

--- src/TeacherDashboard/Footer2.js
+++ src/TeacherDashboard/Footer2.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Footer2Style.css";
+import "./MainTemplate.css";
 
 function MainFooter2() {
   return <footer className="Footer2">© LevelUp Works 2020</footer>;
