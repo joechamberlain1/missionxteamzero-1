95b77f84c63377ef3d569d5c28a442145a4ceb59
strigonone <s15jaychao@hotmail.com>
2021-03-26 00:01:50 +1300

    Cleaned App and made Router on its own

--- src/App.js
+++ src/App.js
@@ -1,73 +1,13 @@
 import React from "react";
-import { Route } from "react-router-dom";
 import "./App.css";
-
-import HomePage from "./Components/HomePage";
-import ImageAvatars from "./Components/ImageAvatars";
-
-import TeacherStudentProfileMain from "./TeacherStudentProfiles/TeacherStudentProfileMain";
-import ProfilePage from "./ProfilePage/ProfilePage";
-
-import LogIn from "./LogInSignUp/Login";
-import SignUp from "./LogInSignUp/SignUp.js";
-import StudentProjectLib from "./StudentProjectLibrary/StudentProjectLib.js";
-import ProjectBuilderSideBar from "./StudentProjectBuilder/ProjectBuilderSideBar";
-import StudentProjectBuilder from "./StudentProjectBuilder/StudentProjectBuilder";
-
-import HelpRequest from "./ofa/help-request";
-import ProgressTracker from "./ofa/progress-tracker";
-import DashboardProgress from "./ofa/dashboard-progress";
-import DashboardHelp from "./ofa/dashboard-help";
-// import Checkboxes from "./ofa/checkbox";
-import RadioButton from "./ofa/radio-button";
-import MainDashProgress from "./ofa/mainDash-progress";
-import MainDashHelp from "./ofa/mainDash-help";
-import LeftNavL2 from "./ofa/leftNavLayout2";
-import TeacherProjectsPage from "./TeacherProjects/TeacherProjectsPage";
-import MainTemplate from "./TeacherDashboard/MainTemplate";
+import Router from "./Router.js";
 
 function App() {
-  return (
-    <div className="App">
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/ProfilePage" component={ProfilePage} />
-      <Route exact path="/Login" component={LogIn} />
-      <Route exact path="/SignUp" component={SignUp} />
-      <Route exact path="/StudentProjectLib" component={StudentProjectLib} />
-      <Route exact path="/dashboard-progress" component={DashboardProgress} />
-      <Route exact path="/progress-tracker" component={ProgressTracker} />
-      <Route exact path="/radio-button" component={RadioButton} />
-      <Route exact path="/help-request" component={HelpRequest} />
-      <Route exact path="/ImageAvatars" component={ImageAvatars} />
-      {/* <Route exact path="/checkbox" component={Checkboxes} /> */}
-      <Route exact path="/dashboard-help" component={DashboardHelp} />
-      <Route exact path="/leftNavLayout2" component={LeftNavL2} />
-      <Route exact path="/mainDash-progress" component={MainDashProgress} />
-      <Route exact path="/mainDash-help" component={MainDashHelp} />
-      <Route
-        exact
-        path="/ProjectBuilderSideBar"
-        component={ProjectBuilderSideBar}
-      />
-
-      <Route
-        exact
-        path="/TeacherStudentProfileMain"
-        component={TeacherStudentProfileMain}
-      />
-      <Route
-        exact
-        path="/StudentProjectBuilder"
-        component={StudentProjectBuilder}
-      />
-      <Route
-        exact
-        path="/TeacherProjectsPage"
-        component={TeacherProjectsPage}
-      />
-      <Route exact path="/maintemplate" component={MainTemplate} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router />
+		</div>
+	);
 }
 
 export default App;
