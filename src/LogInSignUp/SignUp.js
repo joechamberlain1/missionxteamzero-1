e2cad84dc4fee38aec4203b6d006c807b5e5ddb6
joechamberlain1 <joechamberlain2801@gmail.com>
2021-03-28 18:07:37 +1300

    updated sign up to hold some data

--- src/LogInSignUp/SignUp.js
+++ src/LogInSignUp/SignUp.js
@@ -1,57 +1,24 @@
 import React from 'react';
 import './SignUp.css';
-import students from '../Students.jpg';
-import teachers from '../teacher-1.jpg';
-import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
-import Login from './Login.js'
-// import SignUpModal from './SignUpModal.js'
- 
-function SignUp(){
-    return(
+import SignUpTeacher from './SignUpTeachers'
+import SignUpStudent from './SignUpStudent'
 
-        <div className ='Form'>
-          
-            <div className='StudentSignUp'>
-            <img src={students} className='FormPic' alt='Students' />
-            <p>Students</p>
-        {/* these a tags will be changed to Link once we have added in the newer pages */}
-        <Link className = 'FormLinks'>Log In</Link> <Link className = 'FormLinks'>Sign Up</Link>
+function SignUp() {
 
-        <div className='UserInput'>
-        <input placeholder='Full Name'></input>
-        <input placeholder='Email Address' name='email' required></input>
-        <input placeholder='Password'></input>
-        <input placeholder='Confirm Password'></input>
+  return (
 
-          {/* have added link to the student project library page. User data has not been added yet */}
-          </div>
-          <Link><Button className ='LogSignButton' fullWidth = {true} color = 'secondary' variant = 'contained'>Sign Up</Button></Link>
-        
-        
+    <div className='Form'>
+
+      <div className='StudentSignUp'>
+      <SignUpStudent />
+
       </div>
 
       <div className='TeacherSignUp'>
-        <img src={teachers} className="FormPic" alt="Teacher" />
-        <p>Teachers</p>
-        {/* these a tags will be changed to Link once we have added in the newer pages */}
-        <div className='FormLink'>
-        <Link className = 'FormLinks'>Log In</Link> <Link className = 'FormLinks'>Sign Up</Link>
-        </div>
-        <div className='UserInput'>
-        
-        <input placeholder='Full Name'></input>
-        <input placeholder='Email Address' name='email' required></input>
-        <input placeholder='Password'></input>
-        <input placeholder='Confirm Password'></input>
-          {/* Will be changed to teachers page once the page has been made */}
-          </div>
-          <Link><Button className ='LogSignButton' onClick = {<Login />} fullWidth = {true} color = 'secondary' variant = 'contained'>Sign Up</Button></Link>
-          
-         
-        </div>
-        </div>
-      
+      <SignUpTeacher />
+      </div>
+      </div>
+
     )
 }
 
---
+++ src/LogInSignUp/SignUpStudent.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import students from '../Students.jpg';
+import { Link } from 'react-router-dom';
+
+function SignUpStudent() {
+    const { register, handleSubmit, errors, watch } = useForm();
+  
+
+  
+    const onSubmitStudent = data => {
+      console.log(data);
+    }
+  
+  
+    return (
+  
+
+<div className='StudentSignUp'>
+        <img src={students} className='FormPic' alt='Students' />
+        <p>Students</p>
+        {/* these a tags will be changed to Link once we have added in the newer pages */}
+        <Link to='/Login' className='FormLinks'>Log In</Link> <Link className='FormLinks'>Sign Up</Link>
+
+        <form action = 'student register' onSubmit={handleSubmit(onSubmitStudent)} className='UserInput' >
+          <input placeholder='Full Name' type='text' name='fullName' required ref ={register}/>
+          {errors.fullName && (<h6>This is required</h6>)}
+          <input placeholder='Email Address' type='text' name='email' required ref ={register}/>
+          <input placeholder='Password' type='password' name='password' ref={register({ required:true, minLength: 8 })} />
+          <input placeholder='Confirm Password' 
+                 type='password' 
+                 name='confirmPassword' 
+                 ref={register({validate: (value)=>value === watch('password')})} />
+          {/* <Link><input type='submit'><Button className ='LogSignButton' fullWidth = {true} color = 'secondary' variant = 'contained'>Sign Up</Button></input></Link> */}
+          <input type='Submit' value ='Register'/>
+          {/* have added link to the student project library page. User data has not been added yet */}
+        </form>
+
+        <br/>
+
+      </div>
+    )
+}
+
+export default SignUpStudent
