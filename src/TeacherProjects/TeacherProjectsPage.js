626eef85fed067238ef1f11fe94e4f755c6f03fa
strigonone <s15jaychao@hotmail.com>
2021-03-28 22:38:56 +1300

    Adjusting Project library page

--- src/TeacherProjects/TeacherProjectsPage.js
+++ src/TeacherProjects/TeacherProjectsPage.js
@@ -3,48 +3,49 @@ import Nav from "../Components/nav.js";
 import MainFooter from "../MainFooter/Footer.js";
 import "./TeacherProjectPageStyle.css";
 import ProjectGridImg from "./ProjectGridImg.js";
-// import TeacherProjectButtonsTop from "./TeacherProjectButtonsTop";
-import LibButton from '../StudentProjectLibrary/ProjectLibButtons';
-import ProjectSideBar from '../StudentProjectLibrary/ProjectGallerySideBar';
+import LibButton from "../StudentProjectLibrary/ProjectLibButtons";
+import ProjectSideBar from "../StudentProjectLibrary/ProjectGallerySideBar";
+import TeacherProjectBottomButtons from "./TeacherProjectBottomButtons.js";
 
 function TeacherProjectsPage() {
-  return (
-    <div>
-      <Nav />
+	return (
+		<div>
+			<Nav />
 
-      <div className="main-projectcontainer">
-        <div className="projectSideBar">
-          <ProjectSideBar />
-        </div>
-        <div className="projectContent">
-          <h1>Projects</h1>
-          <h4>
-            Welcome to the project library. You can use the filters o nthe right
-            to help you search for specific projects.
-          </h4>
-          <LibButton />
+			<div className="main-projectcontainer">
+				<div className="projectSideBar">
+					<ProjectSideBar />
+				</div>
+				<div className="projectContent">
+					<h1>Projects</h1>
+					<h4>
+						Welcome to the project library. You can use the filters on the right
+						to help you search for specific projects.
+					</h4>
+					<LibButton />
 
-          <div className="ProjectItems">
-            <ProjectGridImg />
-            <ProjectGridImg />
-            <ProjectGridImg />
-          </div>
-          <div className="ProjectItems">
-            <ProjectGridImg />
-            <ProjectGridImg />
-            <ProjectGridImg />
-          </div>
-          <div className="ProjectItems">
-            <ProjectGridImg />
-            <ProjectGridImg />
-            <ProjectGridImg />
-          </div>
-        </div>
-      </div>
+					<div className="ProjectItems">
+						<ProjectGridImg />
+						<ProjectGridImg />
+						<ProjectGridImg />
+					</div>
+					<div className="ProjectItems">
+						<ProjectGridImg />
+						<ProjectGridImg />
+						<ProjectGridImg />
+					</div>
+					<div className="ProjectItems">
+						<ProjectGridImg />
+						<ProjectGridImg />
+						<ProjectGridImg />
+					</div>
+					<TeacherProjectBottomButtons />
+				</div>
+			</div>
 
-      <MainFooter />
-    </div>
-  );
+			<MainFooter />
+		</div>
+	);
 }
 
 export default TeacherProjectsPage;
