054ccc76a8bf926efb07f0615974ea88ff3cb4ff
strigonone <s15jaychao@hotmail.com>
2021-03-28 23:12:10 +1300

    Have merged into 1 componenet.

--- src/TeacherDashboard/DashboardTopButtons.js
+++ src/TeacherDashboard/DashboardTopButtons.js
@@ -1,17 +1,18 @@
 import React from "react";
-import HelpCenterButton from "./HelpCenterButton";
-import MoreProjectsButton from "./MoreProjectsButton";
-import TakeScreenShotButton from "./TakeScreenShotButton";
+import "./ButtonStyle.css";
+import { Link } from "react-router-dom";
 import "./MainTemplate.css";
 
 function DashboardTopButtons() {
-  return (
-    <div className="DashboardButtonContainer">
-      <TakeScreenShotButton />
-      <HelpCenterButton />
-      <MoreProjectsButton />
-    </div>
-  );
+	return (
+		<div className="DashboardButtonContainer">
+			<button className="TakeScreenshot">Take Screenshot</button>;
+			<button className="helpcenterbutton">Help Center</button>;
+			<button className="moreprojectsbutton">
+				<Link to="/TeacherProjectsPage">More Projects</Link>
+			</button>
+		</div>
+	);
 }
 
 export default DashboardTopButtons;
