e56b22585117ab1ded33675deb695bcd2539be2d
strigonone <s15jaychao@hotmail.com>
2021-03-29 23:16:09 +1300

    Updated TeacherProjectPage

--- src/TeacherProjects/ProjectGridImg.js
+++ src/TeacherProjects/ProjectGridImg.js
@@ -1,15 +1,15 @@
 import React from "react";
-import project1 from "../img/Project -1@2x.jpg";
+import project1 from "../img/project-icons/Projectvid01.png";
 import "./TeacherProjectPageStyle.css";
 
 function ProjectGridImg() {
-  return (
-    <div className="ProjectGridImg">
-      <img src={project1} className="ProjectPic1" alt="Project 1" />
-      <h2>Video Name Function</h2>
-      <p>Level of content function</p>
-    </div>
-  );
+	return (
+		<div className="ProjectGridImg">
+			<img src={project1} className="ProjectPic" alt="Project 1" />
+			<h2>Video Name Function</h2>
+			<p>Level of content function</p>
+		</div>
+	);
 }
 
 export default ProjectGridImg;
