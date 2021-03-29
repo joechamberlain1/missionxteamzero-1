9efd37d77b1ef922a1ab710634d4747c3e90efb0
strigonone <s15jaychao@hotmail.com>
2021-03-29 23:15:16 +1300

    TopNav for Dashboard compressed and named properly

--- src/TeacherStudentProfiles/TeacherStudentProfileMain.js
+++ src/TeacherStudentProfiles/TeacherStudentProfileMain.js
@@ -3,47 +3,47 @@ import "../TeacherDashboard/MainTemplate.css";
 import MainFooter2 from "../TeacherDashboard/Footer2";
 
 import StudentProfileGrid from "./StudentProfileGrid";
-import TopNav3 from "../TeacherDashboard/TopNavPart3";
+import DashboardTopNav from "../TeacherDashboard/DashboardTopNav";
 import DashboardTopButtons from "../TeacherDashboard/DashboardTopButtons";
 import SideNav from "../TeacherDashboard/SideNavigation/SideNav";
 
 function TeacherStudentProfileMain() {
-  return (
-    <div className="webcontainer">
-      <TopNav3 />
-      <div className="profile-main-container">
-        <SideNav />
-        <div className="DashboardContent">
-          <DashboardTopButtons />
-          <div className="profile-maininner-container">
-            <div className="grid-container">
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-            </div>
-            <div className="grid-container">
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-            </div>
-            <div className="grid-container">
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-              <StudentProfileGrid />
-            </div>
-          </div>
-        </div>
-      </div>
+	return (
+		<div className="webcontainer">
+			<DashboardTopNav />
+			<div className="profile-main-container">
+				<SideNav />
+				<div className="DashboardContent">
+					<DashboardTopButtons />
+					<div className="profile-maininner-container">
+						<div className="grid-container">
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+						</div>
+						<div className="grid-container">
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+						</div>
+						<div className="grid-container">
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+							<StudentProfileGrid />
+						</div>
+					</div>
+				</div>
+			</div>
 
-      <MainFooter2 />
-    </div>
-  );
+			<MainFooter2 />
+		</div>
+	);
 }
 
 export default TeacherStudentProfileMain;
