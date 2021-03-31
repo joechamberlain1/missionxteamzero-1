948506ef20afe90c7218fc2052b651fc98f1a395
strigonone <s15jaychao@hotmail.com>
2021-03-31 20:57:20 +1300

    Updated pictures and moved to allocated folder

--- src/LogInSignUp/SignUpStudent.js
+++ src/LogInSignUp/SignUpStudent.js
@@ -1,43 +1,61 @@
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import students from '../Students.jpg';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useForm } from "react-hook-form";
+import students from "../img/LoginSignUp Modal/Students.png";
+import { Link } from "react-router-dom";
 
 function SignUpStudent() {
-    const { register, handleSubmit, errors, watch } = useForm();
-  
-    const onSubmitStudent = data => {
-      console.log(data);
-    }
-  
-  
-    return (
-  
+	const { register, handleSubmit, errors, watch } = useForm();
 
-<div className='StudentSignUp'>
-        <img src={students} className='FormPic' alt='Students' />
-        <p>Students</p>
-        {/* these a tags will be changed to Link once we have added in the newer pages */}
-        <Link className='FormLinks'>LOG IN</Link> <Link className='FormLinks'>SIGN UP</Link>
+	const onSubmitStudent = (data) => {
+		console.log(data);
+	};
 
-        <form action = 'student register' onSubmit={handleSubmit(onSubmitStudent)} className='UserInput' >
-          <input placeholder='Full Name' type='text' name='fullName' required ref ={register}/>
-          {errors.fullName && (<h6>This is required</h6>)}
-          <input placeholder='Email Address' type='text' name='email' required ref ={register}/>
-          <input placeholder='Password' type='password' name='password' ref={register({ required:true, minLength: 8 })} />
-          <input placeholder='Confirm Password' 
-                 type='password' 
-                 name='confirmPassword' 
-                 ref={register({validate: (value)=>value === watch('password')})} />
-          {/* <Link><input type='submit'><Button className ='LogSignButton' fullWidth = {true} color = 'secondary' variant = 'contained'>Sign Up</Button></input></Link> */}
-          <input type='Submit' value ='Register'/>
-          {/* have added link to the student project library page. User data has not been added yet */}
-        </form>
-
-        <br/>
-
-      </div>
-    )
+	return (
+		<div className="StudentSignUp">
+			<img src={students} className="FormPic" alt="Students" />
+			<p>Students</p>
+			{/* these a tags will be changed to Link once we have added in the newer pages */}
+			<Link className="FormLinks">LOG IN</Link>{" "}
+			<Link className="FormLinks">SIGN UP</Link>
+			<form
+				action="student register"
+				onSubmit={handleSubmit(onSubmitStudent)}
+				className="UserInput"
+			>
+				<input
+					placeholder="Full Name"
+					type="text"
+					name="fullName"
+					required
+					ref={register}
+				/>
+				{errors.fullName && <h6>This is required</h6>}
+				<input
+					placeholder="Email Address"
+					type="text"
+					name="email"
+					required
+					ref={register}
+				/>
+				<input
+					placeholder="Password"
+					type="password"
+					name="password"
+					ref={register({ required: true, minLength: 8 })}
+				/>
+				<input
+					placeholder="Confirm Password"
+					type="password"
+					name="confirmPassword"
+					ref={register({ validate: (value) => value === watch("password") })}
+				/>
+				{/* <Link><input type='submit'><Button className ='LogSignButton' fullWidth = {true} color = 'secondary' variant = 'contained'>Sign Up</Button></input></Link> */}
+				<input type="Submit" value="Register" />
+				{/* have added link to the student project library page. User data has not been added yet */}
+			</form>
+			<br />
+		</div>
+	);
 }
 
-export default SignUpStudent
+export default SignUpStudent;
--- src/LogInSignUp/SignUpTeachers.js
+++ src/LogInSignUp/SignUpTeachers.js
@@ -1,38 +1,58 @@
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import teachers from '../teacher-1.jpg';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useForm } from "react-hook-form";
+import teachers from "../img/LoginSignUp Modal/teacher-1.png";
+import { Link } from "react-router-dom";
 
-const SignUpTeacher= () => {
+const SignUpTeacher = () => {
+	const { register, handleSubmit, errors, watch } = useForm();
 
-    const { register, handleSubmit, errors, watch } = useForm();
+	const onSubmitTeacher = (data) => {
+		console.log(data);
+	};
 
-    const onSubmitTeacher = data => {
-      console.log(data);
-    }
-  
-    return(
-        <div>
-        <img src={teachers} className="FormPic" alt="Teacher" />
-        <p>Teachers</p>
-        {/* these a tags will be changed to Link once we have added in the newer pages */}
-        <Link to='/Login' className='FormLinks'>Log In</Link> <Link className='FormLinks'>Sign Up</Link>
+	return (
+		<div>
+			<img src={teachers} className="FormPic" alt="Teacher" />
+			<p>Teachers</p>
+			{/* these a tags will be changed to Link once we have added in the newer pages */}
+			<Link to="/Login" className="FormLinks">
+				Log In
+			</Link>{" "}
+			<Link className="FormLinks">Sign Up</Link>
+			<form onSubmit={handleSubmit(onSubmitTeacher)} className="UserInput">
+				<input
+					placeholder="Full Name"
+					type="text"
+					name="fullName"
+					required
+					ref={register}
+				/>
+				{errors.fullName && <h6>This is required</h6>}
+				<input
+					placeholder="Email Address"
+					type="text"
+					name="email"
+					required
+					ref={register({ required: true })}
+				/>
+				{errors.email && <h6>This is required</h6>}
+				<input
+					placeholder="Password"
+					type="password"
+					name="password"
+					ref={register({ required: "Password is required", minLength: 8 })}
+				/>
+				<input
+					placeholder="Confirm Password"
+					type="password"
+					name="confirmPassword"
+					ref={register({ validate: (value) => value === watch("password") })}
+				/>
+				{errors.password && <h6>{errors.password.message}</h6>}
+				<input type="Submit" value="Register" />
+			</form>
+		</div>
+	);
+};
 
-        <form onSubmit={handleSubmit(onSubmitTeacher)} className='UserInput'>
-            <input placeholder='Full Name' type='text' name='fullName' required ref ={register}/>
-            {errors.fullName && <h6>This is required</h6>}
-            <input placeholder='Email Address' type='text' name='email' required ref ={register({required: true})}/>
-            {errors.email && <h6>This is required</h6>}
-            <input placeholder='Password' type='password' name='password' ref={register({ required: "Password is required", minLength: 8 })} />
-            <input placeholder='Confirm Password' 
-                 type='password' 
-                 name='confirmPassword' 
-                 ref={register({validate: (value)=>value === watch('password')})} />
-            {errors.password && <h6>{errors.password.message}</h6>}
-            <input type='Submit' value ='Register' />
-        </form>
-        </div>
-    )
-}
-
-export default SignUpTeacher
+export default SignUpTeacher;
