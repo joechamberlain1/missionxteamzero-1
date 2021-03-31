86a82f47768365d533c758ac0c2eee15f8288b7f
strigonone <s15jaychao@hotmail.com>
2021-04-01 00:10:21 +1300

    Styling update

--- src/TeacherDashboard/SideNavigation/SideNav.js
+++ src/TeacherDashboard/SideNavigation/SideNav.js
@@ -8,85 +8,85 @@ import SideNavBottom from "./SideNavBottom";
 import * as RiIcons from "react-icons/ri";
 
 function SideNav() {
-  const [sidebar, setSidebar] = useState(false);
+	const [sidebar, setSidebar] = useState(false);
 
-  const showSideBar = () => {
-    setSidebar(true);
-  };
-  const hideSideBar = () => {
-    setSidebar(false);
-  };
+	const showSideBar = () => {
+		setSidebar(true);
+	};
+	const hideSideBar = () => {
+		setSidebar(false);
+	};
 
-  // Collapsed Sidebar data
-  const showCollapsedSideBar = () => {
-    return (
-      <div className="SideBarCollapsed">
-        <div className="ProfilePicPlacementSideBar">
-          <StudentProfileGridPic />
-        </div>
+	// Collapsed Sidebar data
+	const showCollapsedSideBar = () => {
+		return (
+			<div className="SideBarCollapsed">
+				<div className="ProfilePicPlacementSideBar">
+					<StudentProfileGridPic />
+				</div>
 
-        <div className="SideBarTopContentCollapsed">
-          {SideNavData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={item.path}>{item.icon}</Link>
-              </li>
-            );
-          })}
-        </div>
+				<div className="SideBarTopContentCollapsed">
+					{SideNavData.map((item, index) => {
+						return (
+							<li key={index} className={item.cName}>
+								<Link to={item.path}>{item.icon}</Link>
+							</li>
+						);
+					})}
+				</div>
 
-        <div className="SideBarButton">
-          <Link to="#" className="menuArrows">
-            <RiIcons.RiArrowRightSFill onClick={showSideBar} />
-          </Link>
-        </div>
+				<div className="SideBarButtonCollapsed">
+					<Link to="#" className="menuArrowsCollapsed">
+						<RiIcons.RiArrowRightSFill onClick={showSideBar} />
+					</Link>
+				</div>
 
-        {/* Manually added the collapsed icon so they appear in a collumn */}
-        <div className="SideBarBottomCollapsed">
-          {SideNavBottomData.map((item, index) => {
-            return (
-              <li key={index} className={item.BottomIconText}>
-                <Link to={item.path}>{item.icon}</Link>
-              </li>
-            );
-          })}
-        </div>
-      </div>
-    );
-  };
+				{/* Manually added the collapsed icon so they appear in a collumn */}
+				<div className="SideBarBottomCollapsed">
+					{SideNavBottomData.map((item, index) => {
+						return (
+							<li key={index} className={item.BottomIconText}>
+								<Link to={item.path}>{item.icon}</Link>
+							</li>
+						);
+					})}
+				</div>
+			</div>
+		);
+	};
 
-  // Main Sidebar when it is opened
-  const showMainSideBar = () => {
-    return (
-      <nav className="SideBarMainContainer active">
-        <div className="ProfilePicPlacementSideBar">
-          <StudentProfileGridPic />
-        </div>
+	// Main Sidebar when it is opened
+	const showMainSideBar = () => {
+		return (
+			<nav className="SideBarMainContainer active">
+				<div className="ProfilePicPlacementSideBar">
+					<StudentProfileGridPic />
+				</div>
 
-        <div className="SideBarTopContent">
-          {SideNavData.map((item, index) => {
-            return (
-              <li key={index} className={item.cName}>
-                <Link to={item.path}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </div>
+				<div className="SideBarTopContent">
+					{SideNavData.map((item, index) => {
+						return (
+							<li key={index} className={item.cName}>
+								<Link to={item.path}>
+									{item.icon}
+									<span>{item.title}</span>
+								</Link>
+							</li>
+						);
+					})}
+				</div>
 
-        <div className="SideBarButton">
-          <Link to="#" className="menuArrows">
-            <RiIcons.RiArrowLeftSFill onClick={hideSideBar} />
-          </Link>
-        </div>
-        <SideNavBottom />
-      </nav>
-    );
-  };
+				<div className="SideBarButton">
+					<Link to="#" className="menuArrows">
+						<RiIcons.RiArrowLeftSFill onClick={hideSideBar} />
+					</Link>
+				</div>
+				<SideNavBottom />
+			</nav>
+		);
+	};
 
-  return <>{sidebar ? showMainSideBar() : showCollapsedSideBar()}</>;
+	return <>{sidebar ? showMainSideBar() : showCollapsedSideBar()}</>;
 }
 
 export default SideNav;
--- src/TeacherDashboard/SideNavigation/SideNavBottomData.js
+++ src/TeacherDashboard/SideNavigation/SideNavBottomData.js
@@ -4,22 +4,22 @@ import * as FaIcons from "react-icons/fa";
 import * as CgIcons from "react-icons/cg";
 
 export const SideNavBottomData = [
-  {
-    title: "Profile",
-    path: "/profilepage",
-    icon: <CgIcons.CgProfile />,
-    cName: "BottomIconText",
-  },
-  {
-    title: "Settings",
-    // path: "/",
-    icon: <FaIcons.FaCog />,
-    cName: "BottomIconText",
-  },
-  {
-    title: "Log out",
-    // path: "/",
-    icon: <FiIcons.FiLogOut />,
-    cName: "BottomIconText",
-  },
+	{
+		title: "Profile",
+		path: "/profilepage",
+		icon: <CgIcons.CgProfile />,
+		cName: "BottomIconText",
+	},
+	{
+		title: "Settings",
+		// path: "/",
+		icon: <FaIcons.FaCog />,
+		cName: "BottomIconText",
+	},
+	{
+		title: "Log out",
+		// path: "/",
+		icon: <FiIcons.FiLogOut />,
+		cName: "BottomIconText",
+	},
 ];
