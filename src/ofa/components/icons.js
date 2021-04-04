a55d9d22c6403f25b5296dc981b6fe8051bf05bb
strigonone <s15jaychao@hotmail.com>
2021-04-05 00:07:34 +1200

    Updating links

--- src/ofa/components/icons.js
+++ src/ofa/components/icons.js
@@ -7,10 +7,20 @@ import subicon from "../img/project-icons/submissions-dark.png";
 import libraryicon from "../img/project-icons/library-light.png";
 
 export const ProIcons = [
-  { title: "", path: "/mainDash-progress", icon: <img src={progressicon} alt="progressicon"/>, cName: "layicon1", },
-  { title: "", path: "/leftNavLayout2", icon: <img src={studenticon} alt="studenticon"/>, cName: "layicon", },
-  { title: "", path: "/mainDash-help", icon: <img src={helpicon} alt="helpicon"/>, cName: "layicon", },
-  { title: "", path: "/leftNavLayout2", icon: <img src={subicon} alt="subicon"/>, cName: "layicon", },
-  { title: "", path: "/leftNavLayout2", icon: <img src={libraryicon} alt="libraryicon"/>, cName: "layicon", },
+	{
+		path: "/mainDash-progress",
+		icon: <img src={progressicon} alt="progressicon" />,
+		cName: "layicon",
+	},
+	{
+		path: "/TeacherStudentProfile",
+		icon: <img src={studenticon} alt="studenticon" />,
+		cName: "layicon",
+	},
+	{ icon: <img src={helpicon} alt="helpicon" />, cName: "layicon1" },
+	{ icon: <img src={subicon} alt="subicon" />, cName: "layicon" },
+	{
+		icon: <img src={libraryicon} alt="libraryicon" />,
+		cName: "layicon",
+	},
 ];
-
--- src/ofa/leftNavHelp.js
+++ src/ofa/leftNavHelp.js
@@ -6,61 +6,47 @@ import { SetIcon } from "./components/icons2";
 import Tavitar from "./components/tavitar";
 import openicon from "../ofa/img/open.png";
 
-
-
 function LeftNavL2() {
+	return (
+		<div className="mainL">
+			<div className="leftnavbody">
+				<Tavitar />
 
-  return (
-    <div className="mainL">
-              <div className="leftnavbody">
-                <Tavitar />
+				{/*left-side menu */}
+				{ProIcons.map((item, index) => {
+					return (
+						<li key={index} className={item.cName}>
+							<Link to={item.path}>
+								{item.icon}
+								<span>{item.title}</span>
+							</Link>
+						</li>
+					);
+				})}
 
+				{/* open button */}
+				<div className="arrowopen1">
+					<Link to="/TeacherStudentProfile">
+						<img src={openicon} alt="Open Icon" />
+					</Link>
+				</div>
 
-                {/*left-side menu */}
-                {ProIcons.map((item, index) => {
-                  return (
-                    <li key={index} className={item.cName}>
-                      <Link to={item.path}>
-                        {item.icon}
-                        <span>{item.title}</span>
-                      </Link>
-                    </li>
-                  );
-                })}
-
-                
-                {/* open button */}     
-                <div className="arrowopen1">
-                  <Link to="/TeacherStudentProfileMain"><img src={openicon} alt="Open Icon"/>
-                  
-                  </Link>
-                </div>
-          
-
-                {/*left-side bottom menu*/}
-                <div className="bottomicon">
-                {SetIcon.map((item, index) => {
-                        return (
-                          <li key={index} className={item.cName}>
-                            <Link to={item.path}>
-                              {item.icon}
-                              <span>{item.title}</span>
-                            </Link>
-                          </li>
-                        );
-                      })}
-                </div>
-                </div>
-    </div>
-  
-  
-  
-  
-  
-  
-  
-  
-  );
+				{/*left-side bottom menu*/}
+				<div className="bottomicon">
+					{SetIcon.map((item, index) => {
+						return (
+							<li key={index} className={item.cName}>
+								<Link to={item.path}>
+									{item.icon}
+									<span>{item.title}</span>
+								</Link>
+							</li>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LeftNavL2;
