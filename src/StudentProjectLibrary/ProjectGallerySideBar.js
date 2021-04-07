1660e71a95569296f2d39597f9266bec094e8656
Joe Chamberlain <you@example.com>
2021-04-08 11:38:37 +1200

    side bar is all one function

--- src/StudentProjectLibrary/ProjectGallerySideBar.js
+++ src/StudentProjectLibrary/ProjectGallerySideBar.js
@@ -5,29 +5,31 @@ import SideBarCheckbox from './checkboxSideBar'
 
 function ProjectSideBar() {
 
-    const [checkboxes  ] = useState([
+    const [checkboxes, setCheckboxes  ] = useState([
 
         { title: 'Subscription', itemTypes: ['Free', 'Premium'], id:1 },
         { title: 'Activity Type', itemTypes: ['Animation', 'Game', 'Chatbot', 'Augmented Reality'], id: 2 },
         { title: 'Year Level', itemTypes: ['1-4', '5-6', '7-8', '9-13'], id:3 },
         { title: 'Subject Matter', itemTypes: ['Computer Science', 'Maths', 'Science', 'Language', 'Art', 'Music'], id:4 },
 
-    ])
+    ]);
         
     return (
         <div>
             {checkboxes.map((checkbox) => (
                 <div className='ProjectSideBar'>
                     <h1 className= 'TableHeader'>{checkbox.title}</h1>
-                   <div>
-                    <SideBarCheckbox className = 'CheckBoxContainer'/>{checkbox.itemTypes[0]}
-                    </div>
+
+                    
+                        
+                        {checkbox.itemTypes.map((item) =>(
+                            <div>
+                            <SideBarCheckbox  className = 'CheckBoxContainer'/>{item}
+                            </div>
+                        ))}
                     
                 </div>    
-                
             )
-                
-                
             )}
 
         </div>
