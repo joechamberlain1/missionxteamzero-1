f86518eaf4bbda1d3faa0091091e821c7b6e3699
strigonone <s15jaychao@hotmail.com>
2021-04-30 18:35:34 +1200

    Removed warnings when compiling

--- src/ProjectLibrary/StudentProjectLibrary/ProjectGallerySideBar.js
+++ src/ProjectLibrary/StudentProjectLibrary/ProjectGallerySideBar.js
@@ -1,44 +1,46 @@
-import React, { useState, } from 'react';
-import './StudentProjectLib.css'
-import SideBarCheckbox from './checkboxSideBar'
-
+import React, { useState } from "react";
+import "./StudentProjectLib.css";
+// import SideBarCheckbox from './checkboxSideBar'
 
 function ProjectSideBar() {
+	const [checkboxes] = useState([
+		{ title: "Subscription", itemTypes: ["Free", "Premium"], id: 1 },
+		{
+			title: "Activity Type",
+			itemTypes: ["Animation", "Game", "Chatbot", "Augmented Reality"],
+			id: 2,
+		},
+		{ title: "Year Level", itemTypes: ["1-4", "5-6", "7-8", "9-13"], id: 3 },
+		{
+			title: "Subject Matter",
+			itemTypes: [
+				"Computer Science",
+				"Maths",
+				"Science",
+				"Language",
+				"Art",
+				"Music",
+			],
+			id: 4,
+		},
+	]);
 
-    const [checkboxes] = useState([
+	return (
+		<div>
+			{checkboxes.map((checkbox) => (
+				<div className="ProjectSideBar">
+					<h1 className="TableHeader">{checkbox.title}</h1>
 
-        { title: 'Subscription', itemTypes: ['Free', 'Premium'], id:1 },
-        { title: 'Activity Type', itemTypes: ['Animation', 'Game', 'Chatbot', 'Augmented Reality'], id: 2 },
-        { title: 'Year Level', itemTypes: ['1-4', '5-6', '7-8', '9-13'], id:3 },
-        { title: 'Subject Matter', itemTypes: ['Computer Science', 'Maths', 'Science', 'Language', 'Art', 'Music'], id:4 },
-
-    ]);
-
-
-
-    return (
-        <div>
-            {checkboxes.map((checkbox) => (
-                <div className='ProjectSideBar'>
-                    <h1 className= 'TableHeader'>{checkbox.title}</h1>
-
-                    
-                        
-                        {checkbox.itemTypes.map((item) =>(
-                            <div>
-                            <input type='checkbox'  className = 'CheckBoxContainer'/>{item}
-                            </div>
-                        ))}
-                    
-                </div>    
-            )
-            )}
-
-        </div>
-    )}
-
+					{checkbox.itemTypes.map((item) => (
+						<div>
+							<input type="checkbox" className="CheckBoxContainer" />
+							{item}
+						</div>
+					))}
+				</div>
+			))}
+		</div>
+	);
+}
 
 export default ProjectSideBar;
-
-
-
